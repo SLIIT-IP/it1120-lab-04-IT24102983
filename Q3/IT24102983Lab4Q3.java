78653325e328fa16e0094775aacb9b933daa8fc5
IT24102983 <IT24102983@my.sliit.lk>
2024-08-23 00:13:50 +0530

    Add IT24102983Lab4Q3.java

---
+++ Q3/IT24102983Lab4Q3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class IT24102983Lab4Q3 {
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+
+System.out.print("Enter a number:");
+int number = scanner.nextInt();
+
+String result;
+result= number>0 ? "Positive" : (number<0? "Negative" : "Zero");
+System.out.println("The number is:" + result);
+        }
+}
